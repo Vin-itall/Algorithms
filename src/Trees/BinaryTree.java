11a2cbe629e07cd01b897d47f11114ce9cc0abac
Vin-itall <vin0itall@gmail.com>
2019-11-15 13:10:34 +0530

    In Pre and Post order traversals

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -1,12 +1,18 @@
 package Trees;
+import java.util.*;
 
 public class BinaryTree {
 	Node Root = new Node(null);
+	int leftDepth =0; 
+	int rightDepth = 0;
+	ArrayList<Node> inorderL = new ArrayList<Node>();
+	ArrayList<Node> preorderL = new ArrayList<Node>();
+	ArrayList<Node> postorderL = new ArrayList<Node>();
 	
 	public void insertNode(int x)
 	{
+		int Count = 0;
 		Node Current = Root;
-		
 		if(Root.Value==null)
 		{
 			Root.Value = x;
@@ -14,11 +20,15 @@ public class BinaryTree {
 		else
 		{
 			Node Next = new Node(null);
-			
 			while(Next.Value == null)
 			{
 				if(x > Current.Value)
 				{
+					if(Count==0)
+					{
+							rightDepth++;
+							Count++;
+					}
 					if(Current.Right != null)
 					{	
 						Current = Current.Right;
@@ -28,11 +38,15 @@ public class BinaryTree {
 						Next.Value = x;
 						Next.Parent = Current;
 						Current.Right = Next;
-						
 					}
 				}
 				else if(x < Current.Value)
 				{
+					if(Count==0)
+					{
+							leftDepth++;
+							Count++;
+					}
 					if(Current.Left != null)
 					{	
 						Current = Current.Left;
@@ -45,10 +59,41 @@ public class BinaryTree {
 					}
 				}
 			}	
+			System.out.println("Left Depth = " + leftDepth +" Right Depth = " + rightDepth);
 		}
 	}
-
 	
+	//inorder is (Left,Root,Right)
+	public void inorderTraversal(Node n)
+	{
+		if(n == null)
+			return;
+		inorderTraversal(n.Left);
+		inorderL.add(n);
+		inorderTraversal(n.Right);
+		
+	}
+	
+	//postorder is (Left,Right,Root)
+	public void postorderTraversal(Node n)
+	{
+		if(n == null)
+			return;
+		postorderTraversal(n.Left);
+		postorderTraversal(n.Right);
+		postorderL.add(n);
+	}
+	
+	//preorder is (Root,Left,Right)
+	public void preorderTraversal(Node n)
+	{	
+		if(n == null)
+			return;
+		preorderL.add(n);
+		preorderTraversal(n.Left);
+		preorderTraversal(n.Right);
+		
+	}
 	
 	public static void main(String args[])
 	{
@@ -57,5 +102,12 @@ public class BinaryTree {
 		BT.insertNode(22);
 		BT.insertNode(10);
 		BT.insertNode(12);
+		BT.inorderTraversal(BT.Root);
+		BT.postorderTraversal(BT.Root);
+		BT.preorderTraversal(BT.Root);
+		for(Node node : BT.preorderL)
+		{
+			System.out.print(node.Value+" ");
+		}
 	}
 }
